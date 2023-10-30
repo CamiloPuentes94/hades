d92b6c0f68031cbcd7fb821226a6868557a535b2
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2023-10-30 10:55:04 -0500

    Se realizan validaciones de formulario de creacion y se agrega libreria de sweetalert2

--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -38,6 +38,9 @@ class CategoryListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Listado de Categorias'
+        context['create_url'] = reverse_lazy('erp:category_create')
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
         return context
 
 class CategoryCreateView(CreateView):
@@ -46,7 +49,22 @@ class CategoryCreateView(CreateView):
     template_name = 'category/create.html'
     success_url = reverse_lazy('erp:category_list')
 
+    # def post(self, request, *args, **kwargs):
+    #     print(request.POST)
+    #     form = CategoryForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(self.success_url)
+    #
+    #     self.object = None
+    #     context = self.get_context_data( **kwargs)
+    #     context['form'] = form
+    #     return render(request, self.template_name, context)
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Creacion una Categoria'
+        context['entity'] = 'Categorias'
+        context['list_url'] = reverse_lazy('erp:category_list')
         return context
